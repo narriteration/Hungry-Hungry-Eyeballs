d70e52aa923fab88f3ec7ce21415a6509c5e676b
narriteration <narriteration@gmail.com>
2017-03-03 19:01:08 -0800

    commented out functions, added img variables within js document so that they appear automatically upon page load

--- scripts/app.js
+++ scripts/app.js
@@ -2,40 +2,64 @@ $(document).ready(function() {
 
     console.log("My JS is connected! Woohoo!")
 
-    $('.messages').html("Game is ready. Click Z or M key to begin race")
+    $('.messages').html("Game is ready. Click Z or M key to begin race");
 
-    var board = $('.board');
-    var messages = $('.messages');
-    var button = $('.btn');
+    var $board = $('.board');
+    var $messages = $('.messages');
+    var $button = $('.btn');
+    var blueImage = '<img src="assets/blueDot.png" alt="a blue dot" height = "20" width = "20" id="blueDot">'
+    var redImage = '<img src="assets/redDot.png" alt="a red dot" height = "20" width = "20" id="redDot">'
 
-    //attach listeners
-
-    function resetGame() {
-        var playerZ = "z"
-        var playerM = "m"
-        $('td').html(''); // clear board
-        console.log("No player spaces are currently active.")
-        $('#startZ').html(playerZ); // put z into starting spot
-        $('#startM').html(playerM); // put m into starting spot
-        $('.messages').html("Game is ready. Click Z or M key to begin race")
-    };
-
-    button.on('click', resetGame());
-    console.log("Ready to start a new game.")
+    $('#startZBlue').html(blueImage); // point to #startZBlue and insert the html for blueImage is
+    $('#startMRed').html(redImage); // point to #startMRed
 
 
-    function showGameStartMessage() {
+    // function resetGame() {
+    //     $('td').html(''); // clear board
+    //     console.log("All spaces are cleared.");
+    //     $('.messages').html("Game is ready. Grab an opponent and click Z or M key to begin racing.")
+    // };
 
-        // if (playerOneKeyAssignment == true) || (playerTwoKeyAssignment == true)
-        console.log("The game has begun!");
-    };
+//     button.on('click', resetGame())
+//
+//     $('body').keydown(function(el){
+//         el.preventDefault();
+//         console.log('You pressed a key!');
+//         if (el.which === 'z') {
+//             $('#blueDot').moveDotRight();
+//         }
+//         if (el.which === 'm') {
+//             $('#redDot').moveDotRight();
+//         }
+//     });
+}); // KEEP. = end of document ready function
+
+
+    // function showGameStartMessage() {
+    //     // if (playerOneKeyAssignment == true) || (playerTwoKeyAssignment == true)
+    //     console.log("The game has begun!");
+    // };
     // Start it up!
-});
 
+// EVERYTHING BELOW = FUNCTIONS
 
+// function movePlayerZ() {
+//   if the start box of PlayerZ is !empty,
+//   then listen for a keypress of z
+//   if keypress z, then moveRight
+//
+// }
+//
+// function moveRight() {
+//   if (hasWon === false) then
+//   for
+//   clear current box
+//   find next sibling box
+//   set html to "z" of that box
+// }
 
-function beginGame() {
-};
-
-function isOver() {
-};
+// function beginGame() {
+// };
+//
+// function isOver() {
+// };
